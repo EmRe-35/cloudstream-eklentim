aab4b2f61f612297bfb019ed1a03fd9d97e5a58a
EmRe-35 <yunusemreakbas@gmail.com>
2026-09-23 02:42:27 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-import com.android.build.gradle.BaseExtension
+8import com.android.build.gradle.BaseExtension
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile

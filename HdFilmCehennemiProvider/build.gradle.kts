1d43e140d71d9863b8dd82d2839a6e82cc889999
EmRe-35 <yunusemreakbas@gmail.com>
2026-09-23 01:58:29 +0300

    Update build.gradle.kts

--- HdFilmCehennemiProvider/build.gradle.kts
+++ HdFilmCehennemiProvider/build.gradle.kts
@@ -1,13 +1,17 @@
-// Eklenti sınıf tanımı cloudstream bloğunun DIŞINDA olmalı
 val pluginClass = "com.example.HdFilmCehennemiPlugin"
 
 version = 1
 
 cloudstream {
     language = "tr"
-    description = "HDFilmCehennemi üzerinden film ve dizi izleme eklentisi"
+    description = "HDFilmCehennemi film ve dizi eklentisi"
     authors = listOf("EmRe-35")
     status = 1
     tvTypes = listOf("Movie", "TvSeries")
     iconUrl = "https://www.hdfilmcehennemi.nl/favicon.ico"
+}
+
+dependencies {
+    implementation("org.jspecify:jspecify:1.0.0")
+
 }

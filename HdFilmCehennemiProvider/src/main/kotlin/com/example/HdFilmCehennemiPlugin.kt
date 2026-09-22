13efef21a994bc7b2356d264e215042ab761d7e0
EmRe-35 <yunusemreakbas@gmail.com>
2026-09-22 07:26:00 +0300

    Update HdFilmCehennemiPlugin.kt

--- HdFilmCehennemiProvider/src/main/kotlin/com/example/HdFilmCehennemiPlugin.kt
+++ HdFilmCehennemiProvider/src/main/kotlin/com/example/HdFilmCehennemiPlugin.kt
@@ -5,8 +5,8 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class ArchiveOrgPlugin : Plugin() {
+class HdFilmCehennemiPlugin : Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(ArchiveOrgProvider())
+        registerMainAPI(HdFilmCehennemiProvider())
     }
 }

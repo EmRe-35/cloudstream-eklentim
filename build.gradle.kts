4b2363477ebb42e1621991e09fbe2eaf506ed298
EmRe-35 <yunusemreakbas@gmail.com>
2026-09-23 01:50:35 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,6 +72,7 @@ subprojects {
         implementation("com.github.Blatzar:NiceHttp:0.4.11")
         implementation("org.jsoup:jsoup:1.18.3")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+        compileOnly("org.jspecify:jspecify:1.0.0")
     }
 }
 

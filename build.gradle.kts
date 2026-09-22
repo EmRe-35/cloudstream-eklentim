c27c62023ce398c7d93045cd4a4e2222293559a2
EmRe-35 <yunusemreakbas@gmail.com>
2026-09-23 02:25:29 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,12 +67,12 @@ subprojects {
         val implementation by configurations
 
         cloudstream("com.lagradost:cloudstream3:pre-release")
-
+        implementation("org.jspecify:jspecify:1.0.0")
         implementation(kotlin("stdlib"))
         implementation("com.github.Blatzar:NiceHttp:0.4.11")
         implementation("org.jsoup:jsoup:1.18.3")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
-        compileOnly("org.jspecify:jspecify:1.0.0")
+    
     }
 }
 

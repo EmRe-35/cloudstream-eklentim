790f41b46e054587936870fa8a8ed5b77d3edc09
EmRe-35 <yunusemreakbas@gmail.com>
2026-09-22 17:17:05 +0300

    Update build.gradle.kts

--- HdFilmCehennemiProvider/build.gradle.kts
+++ HdFilmCehennemiProvider/build.gradle.kts
@@ -1,9 +1,9 @@
+// Eklenti sınıf tanımı cloudstream bloğunun DIŞINDA olmalı
+val pluginClass = "com.example.HdFilmCehennemiProvider"
+
 version = 1
 
 cloudstream {
-    // Cloudstream Gradle eklentisinde ana sınıf tanımı bu şekilde yapılır
-    pluginClass = "com.example.HdFilmCehennemiProvider"
-    
     language = "tr"
     description = "HDFilmCehennemi üzerinden film ve dizi izleme eklentisi"
     authors = listOf("EmRe-35")

fd5f91730bafa29d323cb8eb72bd00c6cc197cbc
EmRe-35 <yunusemreakbas@gmail.com>
2026-09-22 03:57:57 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,57 @@
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+import com.android.build.gradle.BaseExtension
 
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://raw.githubusercontent.com/recloudstream/gradle/master")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "kotlin-android")
+    apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+    extensions.configure<CloudstreamExtension> {
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "kullanici/repo")
+    }
+
+    extensions.configure<BaseExtension> {
+        namespace = "com.example.plugins"
+        compileSdkVersion(33)
+
+        defaultConfig {
+            minSdk = 21
+            targetSdk = 33
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    dependencies {
+        "compileOnly"("com.github.recloudstream:cloudstream:pre-release")
+    }
+}

33f885c6000180f9f30269ecb71dae78b4bfc7bb
EmRe-35 <yunusemreakbas@gmail.com>
2026-09-22 17:07:36 +0300

    Update build.gradle.kts

--- HdFilmCehennemiProvider/build.gradle.kts
+++ HdFilmCehennemiProvider/build.gradle.kts
@@ -1,6 +1,9 @@
 version = 1
 
 cloudstream {
+    // Eklenti ana sınıfının ismi (.kt dosyanızdaki class ismiyle BİREBİR aynı olmalı)
+    setPluginEntry("HdFilmCehennemiProvider")
+    
     language = "tr"
     description = "HDFilmCehennemi üzerinden film ve dizi izleme eklentisi"
     authors = listOf("EmRe-35")

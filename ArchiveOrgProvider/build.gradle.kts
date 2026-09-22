51fad32e09487973016b253a94a3a73266a15217
EmRe-35 <yunusemreakbas@gmail.com>
2026-09-22 03:59:20 +0300

    Update build.gradle.kts

--- ArchiveOrgProvider/build.gradle.kts
+++ ArchiveOrgProvider/build.gradle.kts
@@ -1,1 +1,14 @@
+cloudstream {
+    language = "tr"
+    description = "Archive.org üzerinden telifsiz/açık arşiv içeriği"
+    authors = listOf("SeninAdin")
+    status = 1
+    tvTypes = listOf("Movie")
+    iconUrl = "https://archive.org/favicon.ico"
+}
 
+android {
+    defaultConfig {
+        versionCode = 1
+    }
+}

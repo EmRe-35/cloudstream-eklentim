6ae484616065a9cdc96148b497a8602c6991e8bf
EmRe-35 <yunusemreakbas@gmail.com>
2026-09-22 04:00:30 +0300

    Update ArchiveOrgPlugin.kt

--- ArchiveOrgProvider/src/main/kotlin/com/example/ArchiveOrgPlugin.kt
+++ ArchiveOrgProvider/src/main/kotlin/com/example/ArchiveOrgPlugin.kt
@@ -1,1 +1,12 @@
+package com.example
 
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class ArchiveOrgPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(ArchiveOrgProvider())
+    }
+}

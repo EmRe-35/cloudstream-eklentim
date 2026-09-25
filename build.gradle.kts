a41a3aa8a8c636b4d27793afafab08df140bb4c7
EmRe-35 <yunusemreakbas@gmail.com>
2026-09-25 18:52:23 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
 
@@ -49,7 +49,7 @@ subprojects {
     cloudstream {
         setRepo(
             System.getenv("GITHUB_REPOSITORY")
-                ?: "user/repo"
+                ?: "EmRe-35/cloudstream-eklentim"
         )
     }
 
@@ -82,15 +82,21 @@ subprojects {
 
     dependencies {
         val cloudstream by configurations
-        val implementation by configurations
 
         cloudstream(
             "com.lagradost:cloudstream3:pre-release"
         )
 
         implementation(kotlin("stdlib"))
-        implementation("com.github.Blatzar:NiceHttp:0.4.11")
-        implementation("org.jsoup:jsoup:1.18.3")
+
+        implementation(
+            "com.github.Blatzar:NiceHttp:0.4.11"
+        )
+
+        implementation(
+            "org.jsoup:jsoup:1.18.3"
+        )
+
         implementation(
             "com.fasterxml.jackson.module:" +
                 "jackson-module-kotlin:2.13.1"
